2d8917b9548ee98d60a9db4bf05be630f985937f
Harsh Vardhan <harshvd95@gmail.com>
2015-06-29 21:16:42 +0530

    Adding start connection php

---
+++ start_conn.php
@@ -0,0 +1,23 @@
+<?php 
+
+	function GUID()
+	{
+	    if (function_exists('com_create_guid') === true)
+	    {
+	        return trim(com_create_guid(), '{}');
+	    }
+
+	    return sprintf('%04X%04X-%04X-%04X-%04X-%04X%04X%04X', mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(16384, 20479), mt_rand(32768, 49151), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535));
+	}
+
+	
+	$array = array("guid" => GUID(),"response"=>http_response_code());
+
+	if (isset($_GET["callback"])){
+	echo $_GET["callback"] . '(' . json_encode($array) . ')';
+	}	 
+	else{
+		echo json_encode($array);
+	}
+?>
+	
