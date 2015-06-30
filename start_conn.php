e3f928124c68ce465e63245e6dfd3d8a46fe3f9c
Harsh Vardhan <harshvd95@gmail.com>
2015-06-30 22:02:34 +0530

    changed long -> lng

--- start_conn.php
+++ start_conn.php
@@ -31,7 +31,7 @@
 
         $id_ = $array["guid"];
         $lat_ = $_GET["lat"];
-        $long_= $_GET["long"];
+        $long_= $_GET["lng"];
         $query = "INSERT INTO users (id,latitudes,longitudes,last_update) VALUES ('$id_',$lat_,$long_, NOW())";
         if (mysqli_query($conn, $query)) 
         {
