c97c4ee586e90801fb2ea757504bc098bf16720c
Harsh Vardhan <harshvd95@gmail.com>
2015-07-02 17:59:33 +0530

    updating

--- update_pos.php
+++ update_pos.php
@@ -19,7 +19,7 @@
 	$phone_id = $_GET["guid"];
 	$lat_ = $_GET["lat"];
         $long_= $_GET["lng"];
-        $query = "UPDATE users SET latitudes = $lat_, longitudes = $long_, last_update = NOW() WHERE 'id' = $phone_id";
+        $query = "UPDATE users SET latitudes = $lat_, longitudes = $long_, last_update = NOW() WHERE id = '$phone_id'";
         mysqli_query($conn, $query) OR die($conn_error);		
     }
 
