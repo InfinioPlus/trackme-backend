6be58e28b3f569e12bab6410b6f6ea8fc100aa73
Harsh Vardhan <harshvd95@gmail.com>
2015-07-03 13:20:40 +0530

    correcting spaces

--- update_pos.php
+++ update_pos.php
@@ -16,11 +16,11 @@
 
     if ($conn) {
     
-	$phone_id = $_GET["guid"];
-	$lat_ = $_GET["lat"];
-        $long_= $_GET["lng"];
-        $query1 = "UPDATE users SET latitudes = $lat_, longitudes = $long_, last_update = NOW() WHERE id = '$phone_id'";
-        mysqli_query($conn, $query1) OR die($conn_error);
+		$phone_id = $_GET["guid"];
+		$lat_ = $_GET["lat"];
+	    $long_= $_GET["lng"];
+	    $query1 = "UPDATE users SET latitudes = $lat_, longitudes = $long_, last_update = NOW() WHERE id = '$phone_id'";
+	    mysqli_query($conn, $query1) OR die($conn_error);
 		$query2 = "SELECT channel FROM users WHERE id = '$phone_id'";
 		$arr = array();
 		if($query_run = mysqli_query($conn,$query2))
