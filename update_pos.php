c7f69fe2766731f2f5fb267ae62f2ced8a34215e
Harsh Vardhan <harshvd95@gmail.com>
2015-07-03 13:32:24 +0530

    correcting spaces

--- update_pos.php
+++ update_pos.php
@@ -16,21 +16,21 @@
 
     if ($conn) {
     
-		$phone_id = $_GET["guid"];
-		$lat_ = $_GET["lat"];
-	    $long_= $_GET["lng"];
-	    $query1 = "UPDATE users SET latitudes = $lat_, longitudes = $long_, last_update = NOW() WHERE id = '$phone_id'";
-	    mysqli_query($conn, $query1) OR die($conn_error);
-		$query2 = "SELECT channel FROM users WHERE id = '$phone_id'";
-		$arr = array();
-		if($query_run = mysqli_query($conn,$query2))
-		{
-			while($query_row = mysqli_fetch_assoc($query_run))
-			{
-				$arr['channel'] = $query_row['channel'];				
-			}				
-		}
-		echo json_encode($arr);
+    $phone_id = $_GET["guid"];
+    $lat_ = $_GET["lat"];
+    $long_= $_GET["lng"];
+    $query1 = "UPDATE users SET latitudes = $lat_, longitudes = $long_, last_update = NOW() WHERE id = '$phone_id'";
+    mysqli_query($conn, $query1) OR die($conn_error);
+    $query2 = "SELECT channel FROM users WHERE id = '$phone_id'";
+    $arr = array();
+    if($query_run = mysqli_query($conn,$query2))
+    {
+	while($query_row = mysqli_fetch_assoc($query_run))
+	{
+		$arr['channel'] = $query_row['channel'];				
+	}				
+    }
+    echo json_encode($arr);
     }
 	
 
