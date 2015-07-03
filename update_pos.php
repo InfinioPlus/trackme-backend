4abad7785da0b23bf017bc551c7f7668800dfbee
Harsh Vardhan <harshvd95@gmail.com>
2015-07-03 12:51:19 +0530

    Adding query select in update_pos

--- update_pos.php
+++ update_pos.php
@@ -19,10 +19,26 @@
 	$phone_id = $_GET["guid"];
 	$lat_ = $_GET["lat"];
         $long_= $_GET["lng"];
-        $query = "UPDATE users SET latitudes = $lat_, longitudes = $long_, last_update = NOW() WHERE id = '$phone_id'";
-        mysqli_query($conn, $query) OR die($conn_error);		
+        $query1 = "UPDATE users SET latitudes = $lat_, longitudes = $long_, last_update = NOW() WHERE id = '$phone_id'";
+        mysqli_query($conn, $query1) OR die($conn_error);
+		$query2 = "SELECT * FROM users WHERE id = '$phone_id'";
+		$arr = [];
+		if($query_run = mysqli_query($conn,$query2))
+		{
+			while($query_row = mysqli_fetch_assoc($query_run))
+			{
+				$arry = array();
+				$arry['id'] = $query_row['id'];
+				$arry['channel'] = $query_row['channel'];
+				$arry['latitudes'] = $query_row['latitudes'];
+				$arry['longitudes'] = $query_row['longitudes'];
+				$arry['last_update'] = $query_row['last_update'];
+				$arr.push($arry);				
+			}				
+		}
+		echo json_encode($arr);
     }
-
+	
 
 
 ?>
