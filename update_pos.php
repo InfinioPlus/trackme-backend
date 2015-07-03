2de9474648ead82a349f39f6367d2fa172bbec84
Harsh Vardhan <harshvd95@gmail.com>
2015-07-03 13:16:26 +0530

    Correcting errors

--- update_pos.php
+++ update_pos.php
@@ -21,19 +21,13 @@
         $long_= $_GET["lng"];
         $query1 = "UPDATE users SET latitudes = $lat_, longitudes = $long_, last_update = NOW() WHERE id = '$phone_id'";
         mysqli_query($conn, $query1) OR die($conn_error);
-		$query2 = "SELECT * FROM users WHERE id = '$phone_id'";
-		$arr = [];
+		$query2 = "SELECT channel FROM users WHERE id = '$phone_id'";
+		$arr = array();
 		if($query_run = mysqli_query($conn,$query2))
 		{
 			while($query_row = mysqli_fetch_assoc($query_run))
 			{
-				$arry = array();
-				$arry['id'] = $query_row['id'];
-				$arry['channel'] = $query_row['channel'];
-				$arry['latitudes'] = $query_row['latitudes'];
-				$arry['longitudes'] = $query_row['longitudes'];
-				$arry['last_update'] = $query_row['last_update'];
-				$arr.push($arry);				
+				$arr['channel'] = $query_row['channel'];				
 			}				
 		}
 		echo json_encode($arr);
